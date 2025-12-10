1d0f7a9654fdbee147cd32e400a07a7300144bbf
dashygo097 <dashygo097@github.com>
2025-12-10 19:01:36 +0800

    feat: hpc lib works

--- cmake/SourceCollections.cmake
+++ cmake/SourceCollections.cmake
@@ -1,13 +1,13 @@
 # Collect source and header files
 
 file(GLOB_RECURSE MLCC_SOURCES 
-  ${CMAKE_SOURCE_DIR}/src/*.c 
-  ${CMAKE_SOURCE_DIR}/src/*.cc 
-  ${CMAKE_SOURCE_DIR}/src/*.cpp
+  ${CMAKE_CURRENT_SOURCE_DIR}/src/*.c 
+  ${CMAKE_CURRENT_SOURCE_DIR}/src/*.cc 
+  ${CMAKE_CURRENT_SOURCE_DIR}/src/*.cpp
 )
 
 file(GLOB_RECURSE MLCC_HEADERS 
-  ${CMAKE_SOURCE_DIR}/include/*.h 
-  ${CMAKE_SOURCE_DIR}/include/*.hh 
-  ${CMAKE_SOURCE_DIR}/include/*.hpp
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/*.h 
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/*.hh 
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/*.hpp
 )
--- cmake/config.cmake
+++ cmake/config.cmake
@@ -6,11 +6,3 @@ set(USE_CCACHE ON)
 # Build options
 set(ENABLE_TOOLS_BUILDING ON)
 set(ENABLE_TESTING ON)
-
-# HPC library backend configuration
-set(HPC_ENABLE_SIMD ON CACHE BOOL "Enable SIMD in HPC library")
-set(HPC_ENABLE_OPENMP ON CACHE BOOL "Enable OpenMP in HPC library")
-set(HPC_ENABLE_MPI ON CACHE BOOL "Enable MPI in HPC library")
-set(HPC_ENABLE_CUDA OFF CACHE BOOL "Enable CUDA in HPC library")
-set(HPC_ENABLE_ACCELERATE OFF CACHE BOOL "Enable Accelerate framework in HPC library (macOS)")
-set(HPC_USE_HIGH_LEVEL_OPTIMIZATIONS ON CACHE BOOL "Enable high-level optimizations in HPC")
